69e7d77dcc84892de0642552df069216097d30f4
rafaelw@chromium.org <rafaelw@chromium.org@e5214b95-32ac-14ea-b9af-4214adf2a604>
2011-07-19 21:15:09 +0000

    Add phonecat example
    TBR=adamk
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/7454001
    
    git-svn-id: https://mdv.googlecode.com/svn/trunk@119 e5214b95-32ac-14ea-b9af-4214adf2a604

---
+++ use_cases/phonecat/mdv.js
@@ -0,0 +1,21 @@
+document.write('<link rel="stylesheet" href="../template_element.css">');
+[
+  '../../platform/compat.js',
+  '../../platform/forwarding_handler.js',
+  '../../platform/weak_map.js',
+  '../../platform/observers.js',
+  '../../platform/object_observe.js',
+  '../../path.js',
+  '../../model.js',
+  '../../transform.js',
+  '../../dependency_parser.js',
+  '../../expression_parser.js',
+  '../../place_holder_parser.js',
+  '../../bind_attribute_parser.js',
+  '../../element_model.js',
+  '../../html5_attributes.js',
+  '../../element_bindings.js',
+  '../../template_element.js'
+].forEach(function(src) {
+  document.write('<script src="' + src + '"></script>');
+});
