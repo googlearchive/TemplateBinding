c22bcf76bb4f89f53fd18a7b3f7b694d4c7360d4
Rafael Weinstein <rafaelw@chromium.org>
2013-04-02 16:16:07 -0700

    add hacky call-out for custom element to upgrade during instance creation (cloneNode)
    BUG=
    
    Review URL: https://codereview.appspot.com/8291043

--- src/template_element.js
+++ src/template_element.js
@@ -294,7 +294,13 @@
 
   function createInstance(element, model, delegate) {
     var content = element.ref ? element.ref.content : element.content;
-    return createDeepCloneAndDecorateTemplates(content);
+    var instance = createDeepCloneAndDecorateTemplates(content);
+    // TODO(rafaelw): This is a hack, and is neccesary for the polyfil
+    // because custom elements are not upgraded during cloneNode()
+    if (typeof HTMLTemplateElement.__instanceCreated == 'function') {
+      HTMLTemplateElement.__instanceCreated(instance);
+    }
+    return instance;
   }
 
   mixin(HTMLTemplateElement.prototype, {
