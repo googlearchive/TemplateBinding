a1a365986291e5c109fa0ca0dbc343cc13b973dd
Rafael Weinstein <rafaelw@chromium.org>
2013-04-16 14:58:08 -0700

    more follow-up for IE document.contains
    R=arv
    BUG=
    
    Review URL: https://codereview.appspot.com/8809043

--- src/compat.js
+++ src/compat.js
@@ -34,7 +34,7 @@ var createObject = ('__proto__' in {}) ?
 // IE does not support have Document.prototype.contains.
 if (typeof document.contains != 'function') {
   Document.prototype.contains = function(node) {
-    if (node === this)
+    if (node === this || node.parentNode === this)
       return true;
     return this.documentElement.contains(node);
   }
